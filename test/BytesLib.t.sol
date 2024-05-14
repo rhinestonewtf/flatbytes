86067d09a985f576e4f921d981c1ecb30f3907f5
zeroknots <zeroknots@protonmail.com>
2024-05-14 14:03:00 +0700

    ensure base slopt

--- test/BytesLib.t.sol
+++ test/BytesLib.t.sol
@@ -13,6 +13,12 @@ contract CounterTest is Test {
     function setUp() public {}
 
     function test_store() public {
+
+        bytes32 baseSlot;
+        AssociatedBytesLib.Bytes storage bytesData = data[address(this)];
+        assembly {
+            baseSlot := bytesData.slot
+        }
         bytes memory d = hex"424141414141414141414141414141414141414141414141414141414141414141414143";
         vm.record();
         data[address(this)].store(d);
@@ -26,20 +32,26 @@ contract CounterTest is Test {
         bytes memory _d = data[address(this)].load();
 
         assertEq(_d, d);
+        assertEq(writes[0], baseSlot);
+
     }
 
     function test_mapping() public {
         bytes memory d = hex"424141414141414141414141414141414141414141414141414141414141414141414143";
         vm.record();
         data.store(address(this), d);
-        (bytes32[] memory reads, bytes32[] memory writes) = vm.accesses(address(this));
 
+        bytes memory _d = data.load(address(this));
+        (bytes32[] memory reads, bytes32[] memory writes) = vm.accesses(address(this));
         console.log("writes");
         for (uint256 i = 0; i < writes.length; i++) {
             console.logBytes32(writes[i]);
         }
 
-        bytes memory _d = data.load(address(this));
+        console.log("reads");
+        for (uint256 i = 0; i < reads.length; i++) {
+            console.logBytes32(reads[i]);
+        }
 
         assertEq(_d, d);
     }
